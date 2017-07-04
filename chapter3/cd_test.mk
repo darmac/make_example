1bb8f373887ed9ed7baa030a678644e6214cb79c
darmac <darmac@163.com>
2017-07-05 04:12:57 +0800

    add cmd_test.mk cd_test.mk for chapter3

---
+++ chapter3/cd_test.mk
@@ -0,0 +1,8 @@
+# this is a makefile to test cd and pwd cmd
+
+.PHONY:all
+
+all:
+	@pwd
+	cd ..
+	@pwd
---
+++ chapter3/cmd_test.mk
@@ -0,0 +1,7 @@
+#this is a command test makefile
+
+.PHONY:all
+
+all:
+	@echo "cmd1 process id is :" $$$$
+	@echo "cmd2 process id is :" $$$$
